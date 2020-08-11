952463e26ff6632c82b646e4b7bff71d0f58ba6b
pemenagtti <pedrofcmenegatti@gmail.com>
2020-08-11 15:13:14 -0300

    Primeiro commit

---
+++ src/test/java/tests/InformacoesUsuarioTest.java
@@ -0,0 +1,58 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class InformacoesUsuarioTest {
+    private WebDriver navegador;
+
+    @Before
+    public void setUp() {
+        // Abrindo o navegador
+        System.setProperty("webdriver.chrome.driver", "/Users/pedromenegatti1/drivers/chromedriver");
+        navegador = new ChromeDriver();
+        navegador.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        // Navegando para a página do Taskit:
+        navegador.get("http://www.juliodelima.com.br/taskit");
+    }
+
+    @Test
+    public void testAdicionarUmaInformacaoAdicionalDoUsuario() {
+        // Clicar no link que possui o texte "Sing in"
+        navegador.findElement(By.linkText("Sign in")).click();
+
+        // Identificando o formulário de Login
+        WebElement formularioSignInBox = navegador.findElement(By.id("signinbox"));
+
+        // Digitar no campo com name "login" que está dentro do formulario de id "signinbox" o texto "julio0001"
+        formularioSignInBox.findElement(By.name("login")).sendKeys("julio0001");
+
+        // Digitar no campo com name "password" que está dentro do formulario de id "signinbox" o texto "123456"
+        formularioSignInBox.findElement(By.name("password")).sendKeys("123456");
+
+        // Clicar no link com o texto "SIGN IN"
+        navegador.findElement(By.linkText("SIGN IN")).click();
+
+        // Validar que dentro do elemento com class "me" está o texto "Hi, Julio"
+        WebElement me = navegador.findElement(By.className("me"));
+        String textoNoElementoMe = me.getText();
+        assertEquals("Hi, Julio", textoNoElementoMe);
+
+    }
+
+    @After
+    public void tearDown() {
+        // Fechar o navegador
+        navegador.quit();
+    }
+}
